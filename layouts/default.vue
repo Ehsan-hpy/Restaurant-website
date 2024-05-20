289fab87aeae9e71577880f2227be4dc2b15aeb4
Ehsan <xdehsanxd@gmail.com>
2024-05-20 21:28:10 +0330

    setup

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="flex">
+      <ul>
+        <li>
+          <NuxtLink to="Home">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="About">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="Home">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="Home">Home</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
