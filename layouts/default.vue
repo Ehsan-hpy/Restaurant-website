c0c5cfd46bf66e7fbb9ef49cf1afe5ec2349877c
Ehsan <xdehsanxd@gmail.com>
2024-05-22 12:36:40 +0330

    home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <nav
-    class="flex justify-around items-center w-full h-16 gap-8 z-10 fixed backdrop-blur-lg"
+    class="flex justify-around items-center w-full h-16 gap-8 fixed backdrop-blur-lg"
   >
     <div>
       <img class="w-14 rounded-full image" src="/Logo.jfif" />
     </div>
 
     <ul class="flex gap-10">
-      <li v-for="page in pages" :id="page.name">
+      <li v-for="page in pages" :key="page.name">
         <NuxtLink
           class="px-3 py-2 rounded-md text-gray-200 hover:bg-gray-600 duration-200"
           :to="page.url"
@@ -25,8 +25,8 @@
 const pages = [
   { name: "Home", url: "/" },
   { name: "Menu", url: "/menu" },
-  { name: "Home", url: "/home" },
-  { name: "Home", url: "/home" },
+  { name: "About", url: "/home" },
+  { name: "Contact", url: "/home" },
 ];
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <section class="h-lvh bg-[url('/bg-main.jpg')] bg-cover bg-no-repeat">
-    <h1>Hi</h1>
+  <section
+    class="h-lvh flex items-center justify-center bg-[url('/bg-main.jpg')] bg-cover bg-no-repeat relative -z-20"
+  >
+    <div class="max-w-lg text">
+      <h1 class="text-7xl">
+        Enjoy Our <br />
+        Delicious Meal
+      </h1>
+      <p class="text-xl">
+        Resto is a perfect restaurant that has been established at the 1990, And
+        that's the reason we're professional in our job! Another reason that can
+        prove it is the satisfaction of our thousands or milions customers
+      </p>
+    </div>
+
+    <div class="max-w-md">
+      <img class="pizza-image" src="/main-page-pizza-image.png" alt="pizza" />
+    </div>
   </section>
 </template>
 
@@ -10,11 +26,26 @@
 section::before {
   content: "";
   display: block;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgb(5 19 60 / 70%);
+  z-index: -10;
+}
+
+.pizza-image {
+  animation: image-spin 15s linear infinite;
+}
+
+@keyframes image-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
